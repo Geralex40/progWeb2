2b941a2030bcb7ae1f8cbfb07886bc4165587bb1
Geralex40 <germanacuna011@gmail.com>
2024-08-26 22:43:58 -0600

    Add files via upload
    tarea de js donde se vieron imputs, mensajes, operadores y operadores booleanos

---
+++ 20082024/miProgramaEnJS.js
@@ -0,0 +1,8 @@
+console.log("Programa clase 20082024")
+let valor=prompt("Ingrese un Numero")
+if(valor>=1 && valor<=10){
+    console.log("Correcto, el numero es mayor a 1 y menor a 10")
+}
+else{
+    console.log("Incorrecto")
+}
---
+++ 20082024/miProgramaEnJS2.js
@@ -0,0 +1,15 @@
+console.log("Programa Operadores logicos:")
+let x=1
+let y=0
+
+console.log("Tabla de verdad AND")
+console.log(x && x)
+console.log(x&&y)
+console.log(y&&x)
+console.log(y&&y)
+console.log("--------------------------------")
+console.log("Tabla de verdad OR")
+console.log(x||x)
+console.log(x||y)
+console.log(y||x)
+console.log(y||y)
